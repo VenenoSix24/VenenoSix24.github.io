48fa8dedb44566dfbfcacc937bb80b8362c579ac
VenenoSix24 <3405395460@qq.com>
2023-07-24 15:29:49 +0800

    Site updated: 2023-07-24 15:29:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/07/24/hello-world/","2023/07/24/第一篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/07/24/第一篇文章/","2023/07/24/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
