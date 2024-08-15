79015dc687307c80d1f0a97002bd5aafb2765286
VenenoSix24 <63137181+VenenoSix24@users.noreply.github.com>
2024-08-15 08:39:48 +0000

    Update autodeploy.yml Updated By Github Actions

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["2023/07/26/HuaweiGoogle/","2023/07/24/First-Post/","2023/07/24/Recommend-001/","2023/07/24/hello-world/","2023/07/27/StartUrBlog/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
